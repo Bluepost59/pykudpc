e36ad68ace33b5d5bed8d7a37d8616eaa5b009a7
toratora <kaput.kat@gmail.com>
2017-11-24 11:51:55 +0900

    add function to call queue function directly

--- __init__.py
+++ __init__.py
@@ -1,2 +1,16 @@
 from run import *
 from queue import *
+
+def qs():
+    myq = queue()
+    return myq.qs()
+
+def qstat():
+    myq = queue()
+    return myq.qstat()
+
+def qstatq():
+    myq = queue()
+    return myq.qstatq()
+
+
